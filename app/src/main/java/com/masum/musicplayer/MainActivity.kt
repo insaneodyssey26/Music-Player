10df34c1e1ef77bdcfdcfa56358981634d12b47a
Masum <hydrapikachu29@gmail.com>
2025-05-14 01:03:00 +0530

    Removed unused imports from MainActivity

--- app/src/main/java/com/masum/musicplayer/MainActivity.kt
+++ app/src/main/java/com/masum/musicplayer/MainActivity.kt
@@ -18,9 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.LaunchedEffect
 import com.masum.musicplayer.navigation.AppNavigation
 import com.masum.musicplayer.presentation.viewmodel.MusicViewModel
 import com.masum.musicplayer.presentation.viewmodel.MusicViewModelFactory
